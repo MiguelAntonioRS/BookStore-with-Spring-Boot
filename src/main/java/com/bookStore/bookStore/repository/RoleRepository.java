6f0963a982345f6f34449723ee510b85f92210b6
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-24 11:53:54 -0400

    Creating a Security Configuration for the project

---
+++ src/main/java/com/bookStore/bookStore/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.bookStore.bookStore.repository;
+
+import com.bookStore.bookStore.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
