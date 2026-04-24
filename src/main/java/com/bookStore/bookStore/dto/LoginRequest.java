874f30bc3ba48705980bb7cfb587f630f203dcc8
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-24 11:53:30 -0400

    Creating a Security Configuration for the project

---
+++ src/main/java/com/bookStore/bookStore/dto/LoginRequest.java
@@ -0,0 +1,28 @@
+package com.bookStore.bookStore.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginRequest {
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
