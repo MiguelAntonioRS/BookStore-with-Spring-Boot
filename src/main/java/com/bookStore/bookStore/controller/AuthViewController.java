c8a95679a9786882edec227f00f5e77f8435547b
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-24 12:24:39 -0400

    Creating a Security Configuration for the project

---
+++ src/main/java/com/bookStore/bookStore/controller/AuthViewController.java
@@ -0,0 +1,18 @@
+package com.bookStore.bookStore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthViewController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+}
