bdcb9cb259def2ac8cda9702f66107d8ce6123e6
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-24 11:46:57 -0400

    Feat(entity) finishing the BookEntity class

--- src/main/java/com/bookStore/bookStore/entity/BookEntity.java
+++ src/main/java/com/bookStore/bookStore/entity/BookEntity.java
@@ -9,18 +9,29 @@ import java.util.Objects;
 @Entity
 public class BookEntity {
 
-    // Declaracion de Variables
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String author;
-    private String price;
+    private Double price;
 
-    public BookEntity() { // Constructor vacio
+    public BookEntity() {
     }
 
-    // Metodos
+    public BookEntity(int id, String name, String author, String price) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = Double.parseDouble(price);
+    }
+
+    public BookEntity(int id, String name, String author, Double price) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
     public int getId() {
         return id;
     }
@@ -45,56 +56,11 @@ public class BookEntity {
         this.author = author;
     }
 
-    public String getPrice() {
+    public Double getPrice() {
         return price;
     }
 
-    public void setPrice(String price) {
+    public void setPrice(Double price) {
         this.price = price;
     }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", author='" + author + '\'' +
-                ", price='" + price + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof BookEntity book)) return false;
-        return getId() == book.getId() && Objects.equals(getName(), book.getName()) && Objects.equals(getAuthor(), book.getAuthor()) && Objects.equals(getPrice(), book.getPrice());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName(), getAuthor(), getPrice());
-    }
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
-    }
-
-    public void m() {}
-    public void n() {}
-    public void l() {}
-    public void o() {}
-    public void p() {}
-    public void i() {}
-    public void u() {}
-    public void j() {}
-    public void h() {}
-    public void y() {}
-    public void b() {}
-    public void v() {}
 }
