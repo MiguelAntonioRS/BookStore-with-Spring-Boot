36f272a82b1235d6cee793aa02e0e8fddcfe48b4
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-24 11:54:09 -0400

    Creating a Security Configuration for the project

---
+++ src/main/java/com/bookStore/bookStore/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.bookStore.bookStore.repository;
+
+import com.bookStore.bookStore.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
