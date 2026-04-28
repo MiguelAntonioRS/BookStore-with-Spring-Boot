79ed9a1b8c389c2df9efc6e4b7f15a6043be1040
Miguel <159189630+MiguelAntonioRS@users.noreply.github.com>
2026-04-28 11:52:58 -0400

    Feat(security) Adding the edit profile in the SecurityConfig class

--- src/main/java/com/bookStore/bookStore/repository/BookRepository.java
+++ src/main/java/com/bookStore/bookStore/repository/BookRepository.java
@@ -4,6 +4,10 @@ import com.bookStore.bookStore.entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, Integer> {
+    List<BookEntity> findByNameContainingIgnoreCase(String name);
+    List<BookEntity> findByAuthorContainingIgnoreCase(String author);
 }
